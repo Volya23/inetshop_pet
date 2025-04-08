bc6631541cc8b87d397254079c4df7be1e385f07
Volya23 <v.o.kryzhanivska@gmail.com>
2025-04-08 21:37:22 +0300

    file sign in

---
+++ src/pages/registration/Signin.js
@@ -0,0 +1,16 @@
+import React from "react";
+import BreadCrumb from "../../components/BreadCrumb";
+import Meta from "../../components/Meta";
+
+const Signin = () => {
+  return (
+    <>
+    <Meta title={"Вхід"} />
+    <BreadCrumb title=" Вхід" />
+    <div>Signin</div>
+    </>
+    
+  )
+}
+
+export default Signin;
