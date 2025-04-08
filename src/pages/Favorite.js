f74e741f049b8f968837242bceb9b4801878bb2e
Volya23 <v.o.kryzhanivska@gmail.com>
2025-04-08 09:28:49 +0300

    add favorite page

--- src/pages/Favorite.js
+++ src/pages/Favorite.js
@@ -8,33 +8,27 @@ const Favorite = () => {
       <Meta title={"Улюбленні"} />
       <BreadCrumb title=" Улюбленні" />
 
-    <div className="favorite-wrapper home-wrapper-2 py-5">
-      <div className="container-xxl">
-        <div className="row">
-          <div className="col-2">
-          <div className="favorite-card position-relative">
-                <img
-                    src="images/close.svg"
-                    className="position-absolute cross img-fluid"
-                    alt="cross"
-                />
-                <div className="favorite-card-image">
-                <img
-                        src="images/product/2.png"
-                        className="img-fluid w-100"
-                        alt="product"
-                    />
-                </div>
-                <div className="py-3 px-3 align-items-center">
-                    <h6 className="brand">AD - Spiky</h6>
-                    <h5 className="title">Сухий корм для собак з яловичиною 10 кг</h5>
-                    <h6 className="price">310 грн</h6>
-                </div>
+      <div className="favorite-product-wrapper home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3">
+             <div className="favorite-product-card position-relative">
+                <button type="button" className="btn-close position-absolute top-0 end-0 m-2" aria-label="Close"></button>
+                  <img
+                    src="images/product/2.png"
+                    className="img-fluid"
+                    alt="product"
+                  />
+                      <div className="align-items-center">
+                        <h6 className="brand">AD - Spiky</h6>
+                        <h5 className="title">Сухий корм для собак з яловичиною 10 кг</h5>
+                        <h6 className="price">310 грн</h6>
+                      </div>
+             </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   )
 }
